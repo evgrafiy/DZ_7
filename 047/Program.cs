9b3b1e4d2b86c5246f57130bb97bbc6285dc2a91
Evgrafiy <evgrafiy@outlook.com>
2022-10-25 16:55:44 +0300

    проверка

--- 047/Program.cs
+++ 047/Program.cs
@@ -3,3 +3,44 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+Console.WriteLine($"Мы сейчас расскажем про двумерный массив со случайными вещественными числами");
+
+Console.Write("Загадайте m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Теперь n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+
+double[,] array = new double[m, n];
+
+CreateArrayDouble(array);
+
+WriteArray(array);
+
+Console.WriteLine();
+
+void CreateArrayDouble(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+
+void WriteArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double alignNumber = Math.Round(array[i, j], 1);
+            Console.Write(alignNumber + " ");
+        }
+        Console.WriteLine();
+    }
+}
