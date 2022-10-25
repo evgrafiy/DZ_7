722f053d63eef805592e6a80e2bf84b1f9afd8fe
Evgrafiy <evgrafiy@outlook.com>
2022-10-25 18:42:11 +0300

    Работает

--- 050/Program.cs
+++ 050/Program.cs
@@ -6,59 +6,87 @@
 // 8 4 2 4
 // [1,7] -> такого числа в массиве нет ([1,7] это позиция элемента)
 
-Console.Write("Массив позаимствуем в 47-й задаче. Будет интересно");
-Console.Write("Начните вводить через запятую позиции элемента: ");
-string? positionElement = Console.ReadLine();
-positionElement = RemovingSpaces(positionElement);
-int[] position = ParserString(positionElement);
+//пойдём другим путём
 
-if (position[0] <= m
-&& position[1] <= n
-&& position[0] >= 0
-&& position[1] >= 0)
+Console.WriteLine ("Введите число строк: ");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите число столбцов: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int[,] array = new int [m,n];
+for (int i = 0; i < m; i++)
 {
-    double result = array[position[0] - 1, position[1] - 1];
-    Console.Write($"Значение элемента: {result}");
-}
-else Console.Write($"такого элемента в массиве нет.");
-int[] ParserString(string input)
-{
-    int countNumbers = 1;
-    for (int i = 0; i < input.Length; i++)
+    for (int j = 0; j < n; j++)
     {
-        if (input[i] == ',')
-            countNumbers++;
+        array[i,j] = new Random().Next (-10, 10);
+        Console.Write (array[i,j]+" ");
     }
-
-    int[] numbers = new int[countNumbers];
-
-    int numberIndex = 0;
-    for (int i = 0; i < input.Length; i++)
-    {
-        string subString = String.Empty;
-
-        while (input[i] != ',')
-        {
-            subString += input[i].ToString();
-            if (i >= input.Length - 1)
-                break;
-            i++;
-        }
-        numbers[numberIndex] = Convert.ToInt32(subString);
-        numberIndex++;
-    }
-
-    return numbers;
+    Console.WriteLine();
 }
-string RemovingSpaces(string input)
-{
-    string output = String.Empty;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] != ' ')
-        {
-            output += input[i];
-        }
-    }
-    return output;
-}
+
+Console.WriteLine ("Введите строку элемента: ");
+int row = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите столбец элемента: ");
+int column = Convert.ToInt32 (Console.ReadLine());
+if (row < m && column < n) Console.WriteLine ("Элемент массива с такими позициями существует: "+array[row,column]);
+else Console.WriteLine ("Элемент массива с такими позициями не существует");
+
+
+
+
+// Console.Write("Массив позаимствуем в (№ 47) задаче. Будет интересно.");
+// Console.Write("Начните вводить через запятую позиции элемента: ");
+// string? positionElement = Console.ReadLine();
+// positionElement = RemovingSpaces(positionElement);
+// int[] position = ParserString(positionElement);
+// int[,] array = new int [m,n];
+
+// if (position[0] <= m
+// && position[1] <= n
+// && position[0] >= 0
+// && position[1] >= 0)
+// {
+//     double result = array[position[0] - 1, position[1] - 1];
+//     Console.Write($"Значение элемента: {result}");
+// }
+// else Console.Write($"такого элемента в массиве нет.");
+// int[] ParserString(string input)
+// {
+//     int countNumbers = 1;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ',')
+//             countNumbers++;
+//     }
+
+//     int[] numbers = new int[countNumbers];
+
+//     int numberIndex = 0;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         string subString = String.Empty;
+
+//         while (input[i] != ',')
+//         {
+//             subString += input[i].ToString();
+//             if (i >= input.Length - 1)
+//                 break;
+//             i++;
+//         }
+//         numbers[numberIndex] = Convert.ToInt32(subString);
+//         numberIndex++;
+//     }
+
+//     return numbers;
+// }
+// string RemovingSpaces(string input)
+// {
+//     string output = String.Empty;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] != ' ')
+//         {
+//             output += input[i];
+//         }
+//     }
+//     return output;
+// }
--- 052/Program.cs
+++ 052/Program.cs
@@ -6,3 +6,72 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+int srAr = 0;
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = Convert.ToInt32(new Random().Next(0, 11));
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + "\t  ");
+    Console.WriteLine();
+}
+
+for (int i = 0; i < 3; i++)
+{
+    for (int t = 0; t < 3; t++)
+    {
+        srAr += array[t, i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {(float)srAr / 3}");
+}
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.WriteLine("Загадайте число строк: ");
+//         int m = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("Представьте, сколько столбцов может быть: ");
+//         int n = Convert.ToInt32(Console.ReadLine());
+//         int[,] massive = new int[m, n];
+//         for (int i = 0; i < m; i++)
+//         {
+//             for (int j = 0; j < n; j++)
+//             {
+//                 massive[i, j] = new Random().Next(-10, 10);
+//                 Console.Write(massive[i, j] + " ");
+//             }
+//             Console.WriteLine();
+//         }
+
+//         double result = 0;
+
+//         for (int j = 0; j < n; j++)
+//         {
+//             result = Average(j);
+//             Console.WriteLine(result);
+//         }
+
+//         double Average(int column)
+//         {
+//             double average = 0;
+//             double x = 0;
+//             for (int i = 0; i < m; i++)
+//             {
+//                 average += massive[i, column];
+//             }
+//             x = average / m;
+//             return x;
+//         }
+//     }
+// }
