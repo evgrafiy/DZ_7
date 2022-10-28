3af650b2c6db7865d6fc251d37943b745016dae0
Evgeniy Choopin <evgrafiy@outlook.com>
2022-10-28 17:37:42 +0300

    неясно

--- 050/Program.cs
+++ 050/Program.cs
@@ -8,85 +8,36 @@
 
 //пойдём другим путём
 
-Console.WriteLine ("Введите число строк: ");
-int m = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine ("Введите число столбцов: ");
-int n = Convert.ToInt32 (Console.ReadLine());
-int[,] array = new int [m,n];
-for (int i = 0; i < m; i++)
+Console.WriteLine("Загадайте количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Представьте, сколько столбцов может быть: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++) 
 {
-    for (int j = 0; j < n; j++)
-    {
-        array[i,j] = new Random().Next (-10, 10);
-        Console.Write (array[i,j]+" ");
-    }
-    Console.WriteLine();
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,11));  
 }
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Где ваше число? ");
+    Console.WriteLine();
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Точнее! ");
+ int b = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine();
+ if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
 
-Console.WriteLine ("Введите строку элемента: ");
-int row = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine ("Введите столбец элемента: ");
-int column = Convert.ToInt32 (Console.ReadLine());
-if (row < m && column < n) Console.WriteLine ("Элемент массива с такими позициями существует: "+array[row,column]);
-else Console.WriteLine ("Элемент массива с такими позициями не существует");
-
-
-
-
-// Console.Write("Массив позаимствуем в (№ 47) задаче. Будет интересно.");
-// Console.Write("Начните вводить через запятую позиции элемента: ");
-// string? positionElement = Console.ReadLine();
-// positionElement = RemovingSpaces(positionElement);
-// int[] position = ParserString(positionElement);
-// int[,] array = new int [m,n];
-
-// if (position[0] <= m
-// && position[1] <= n
-// && position[0] >= 0
-// && position[1] >= 0)
-// {
-//     double result = array[position[0] - 1, position[1] - 1];
-//     Console.Write($"Значение элемента: {result}");
-// }
-// else Console.Write($"такого элемента в массиве нет.");
-// int[] ParserString(string input)
-// {
-//     int countNumbers = 1;
-//     for (int i = 0; i < input.Length; i++)
-//     {
-//         if (input[i] == ',')
-//             countNumbers++;
-//     }
-
-//     int[] numbers = new int[countNumbers];
-
-//     int numberIndex = 0;
-//     for (int i = 0; i < input.Length; i++)
-//     {
-//         string subString = String.Empty;
-
-//         while (input[i] != ',')
-//         {
-//             subString += input[i].ToString();
-//             if (i >= input.Length - 1)
-//                 break;
-//             i++;
-//         }
-//         numbers[numberIndex] = Convert.ToInt32(subString);
-//         numberIndex++;
-//     }
-
-//     return numbers;
-// }
-// string RemovingSpaces(string input)
-// {
-//     string output = String.Empty;
-//     for (int i = 0; i < input.Length; i++)
-//     {
-//         if (input[i] != ' ')
-//         {
-//             output += input[i];
-//         }
-//     }
-//     return output;
-// }
