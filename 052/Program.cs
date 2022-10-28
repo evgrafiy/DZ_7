0fab889266184a9f9cbd65f7de073f1b0c6b30a6
Evgeniy Choopin <evgrafiy@outlook.com>
2022-10-28 17:36:10 +0300

    работает

--- 052/Program.cs
+++ 052/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 52
+// Задача 52
 //Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 // Например, задан массив:
 // 1 4 7 2
@@ -34,44 +34,3 @@ for (int i = 0; i < 3; i++)
     Console.WriteLine();
     Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {(float)srAr / 3}");
 }
-
-// internal class Program
-// {
-//     private static void Main(string[] args)
-//     {
-//         Console.WriteLine("Загадайте число строк: ");
-//         int m = Convert.ToInt32(Console.ReadLine());
-//         Console.WriteLine("Представьте, сколько столбцов может быть: ");
-//         int n = Convert.ToInt32(Console.ReadLine());
-//         int[,] massive = new int[m, n];
-//         for (int i = 0; i < m; i++)
-//         {
-//             for (int j = 0; j < n; j++)
-//             {
-//                 massive[i, j] = new Random().Next(-10, 10);
-//                 Console.Write(massive[i, j] + " ");
-//             }
-//             Console.WriteLine();
-//         }
-
-//         double result = 0;
-
-//         for (int j = 0; j < n; j++)
-//         {
-//             result = Average(j);
-//             Console.WriteLine(result);
-//         }
-
-//         double Average(int column)
-//         {
-//             double average = 0;
-//             double x = 0;
-//             for (int i = 0; i < m; i++)
-//             {
-//                 average += massive[i, column];
-//             }
-//             x = average / m;
-//             return x;
-//         }
-//     }
-// }
